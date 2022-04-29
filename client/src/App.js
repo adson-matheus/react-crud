b8a5ef20a2b189847dc16688b92abd9ca3b9c055
Adson <adson.matheus62@gmail.com>
2022-04-29 19:34:33 -0300

    add form frontend #3

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,19 @@ function App() {
   return (
     <div className="App">
       <h1>CRUD</h1>
+      <form method="POST">
+        <div className="p-5">
+          <div className="form-group mb-3">
+            <label for="movie" className="col-sm-2 col-form-label">Movie Name:</label>
+            <input type="text" className="form-control" id="movie"></input>
+          </div>
+          <div className="form-group mb-3">
+            <label for="review">Review:</label>
+            <textarea type="text" className="form-control" id="review" cols='20' rows="10"></textarea>
+          </div>
+        </div>
+        <button className="btn btn-outline-success align-center">Enviar</button>
+      </form>
     </div>
   );
 }
