d765a1afa04b7471c1155fd635265732065acfd3
Adson <adson.matheus62@gmail.com>
2022-04-30 12:04:50 -0300

    consultas no banco, select e insert no mysql #9

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const mysql = require('mysql');
 const app = express();
 
@@ -7,13 +9,23 @@ const db = mysql.createPool({
     user: 'root',
     password: '',
     database: 'movies_db',
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/api/get', (req, res) => {
+    const selectAllMovies = "SELECT * FROM movie_review";
+    db.query(selectAllMovies, (err, result) => {res.send(result);})
 })
 
-app.get('/', (req, res) => {
-    res.send('hello, br');
-    // const insertMovie = "INSERT INTO movie_review (movie, review) VALUES ('Mad Max', '5 Oscars!');";
-    // db.query(insertMovie, (err, result)=> {
-    // })
-});
+app.post('/api/insert', (req, res) => {
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+    const insertMovie = "INSERT INTO movie_review (movie, review) VALUES (?,?);";
+    db.query(insertMovie, [movieName, movieReview], (err, result) => {console.log(result)})
+})
+
 
 app.listen(3001, () => {});
